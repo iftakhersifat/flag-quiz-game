ef34122bbcdd9d356d768ddf6317203d98f1961e
iftakhersifat <hossensifat615@gmail.com>
2025-05-11 15:23:08 +0600

    added updated parts

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     return (
         <div className='container mx-auto flex justify-between mt-10 mb-10'>
             <div>
-                <h1 className='text-3xl font-bold'>Flag Quiz Game</h1>
+                <h1 className='text-3xl font-bold text-amber-500'>Flag Quiz Game</h1>
             </div>
             <div className='space-x-4 flex items-center justify-center'>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About Us</NavLink>
+                <NavLink to="/" className={({isActive})=> isActive ? "text-amber-500 text-[18px] underline" : "text-[18px"}>Home</NavLink>
+                <NavLink to="/about" className={({isActive})=> isActive ? "text-amber-500 text-[18px] underline" : "text-[18px"}>About Us</NavLink>
             </div>
             
         </div>
